4877b1fc7a843636a76a07caa23ac5d590389c86
suhail-sk-epam <102290328+suhail-sk-epam@users.noreply.github.com>
2024-12-12 23:04:25 +0530

    Create feature/automated-tests-2_v2/PageClasses.java

---
+++ feature/automated-tests-2_v2/PageClasses.java
@@ -0,0 +1,18 @@
+public class PageClasses {
+    // Locator for sorting dropdown
+    String sortingDropdownLocator = "//selector-for-sorting-dropdown";
+
+    // Method to select sort by price
+    public void sortByPrice() {
+        // code to click dropdown and select 'Price (Low to High)'
+    }
+
+    // Locator for product prices
+    String productPricesLocator = "//selector-for-product-prices";
+
+    // Method to verify ascending order of prices
+    public boolean verifyPricesInAscendingOrder() {
+        // code to check if prices are in ascending order
+        return true;
+    }
+}
