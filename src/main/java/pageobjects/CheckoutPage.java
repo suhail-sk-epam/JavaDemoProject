e846c1d3fdac5ca11b85a7fb8da994a1ed547479
suhail-sk-epam <102290328+suhail-sk-epam@users.noreply.github.com>
2025-01-09 18:06:46 +0530

    Create src/main/java/pageobjects/CheckoutPage.java

---
+++ src/main/java/pageobjects/CheckoutPage.java
@@ -0,0 +1,37 @@
+package pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CheckoutPage {
+    private WebDriver driver;
+    private By firstNameField = By.id("first-name");
+    private By lastNameField = By.id("last-name");
+    private By zipCodeField = By.id("postal-code");
+    private By continueButton = By.id("continue");
+    private By finishButton = By.id("finish");
+
+    public CheckoutPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void enterFirstName(String firstName) {
+        driver.findElement(firstNameField).sendKeys(firstName);
+    }
+
+    public void enterLastName(String lastName) {
+        driver.findElement(lastNameField).sendKeys(lastName);
+    }
+
+    public void enterZipCode(String zipCode) {
+        driver.findElement(zipCodeField).sendKeys(zipCode);
+    }
+
+    public void clickContinue() {
+        driver.findElement(continueButton).click();
+    }
+
+    public void clickFinish() {
+        driver.findElement(finishButton).click();
+    }
+}
